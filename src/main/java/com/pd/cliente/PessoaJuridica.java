046278cbcf15ac5b6cf7a0a64084a50c5b83ade1
Felipe Neves <felipe@attornatus.com.br>
2019-04-22 23:21:45 -0300

    Adicionado nova propriedade

--- src/main/java/com/pd/cliente/PessoaJuridica.java
+++ src/main/java/com/pd/cliente/PessoaJuridica.java
@@ -1,6 +1,6 @@
 package com.pd.cliente;
 
-import lombok.*;
+import lombok.Builder;
 
 @Builder
 public class PessoaJuridica extends Cliente {
@@ -8,8 +8,17 @@ public class PessoaJuridica extends Cliente {
     private String razaoSocial;
     private String cnpj;
     private Double capital;
+    private boolean situacaoFinanceiraOkEmpresa;
 
-    public boolean isCapitalPositivo() {
+    private boolean isCapitalPositivo() {
         return capital > 0;
+    }
+
+    public void setSituacaoFinanceiraEmpresa() {
+        if (isCapitalPositivo()) {
+            this.situacaoFinanceiraOkEmpresa = Boolean.TRUE;
+        } else {
+            this.situacaoFinanceiraOkEmpresa = Boolean.FALSE;
+        }
     }
 }
