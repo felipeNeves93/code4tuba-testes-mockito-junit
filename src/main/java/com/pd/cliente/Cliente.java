74f4a88d9a00d40c717a106037844c975a869d04
Felipe Neves <felipe@attornatus.com.br>
2019-05-02 22:17:55 -0300

    Removido contas cliente

--- src/main/java/com/pd/cliente/Cliente.java
+++ src/main/java/com/pd/cliente/Cliente.java
@@ -1,14 +1,11 @@
 package com.pd.cliente;
 
-import com.pd.conta.Conta;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
@@ -17,7 +14,6 @@ import java.util.List;
 public abstract class Cliente {
 
     private Long id;
-    private List<Conta> contas = new ArrayList<>();
     private LocalDateTime dataCriacaoCadastro;
     private boolean ativo;
     private LocalDateTime dataEncerramentoCadastro;
