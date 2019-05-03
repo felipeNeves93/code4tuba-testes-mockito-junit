ea7483d8bd8f63b34ae5c37066333baefe7a39c3
Felipe Neves <felipe@attornatus.com.br>
2019-05-02 23:02:57 -0300

    Incrementado teste

--- src/test/java/com/pd/cliente/PessoaJuridicaTest.java
+++ src/test/java/com/pd/cliente/PessoaJuridicaTest.java
@@ -3,7 +3,7 @@ package com.pd.cliente;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
 
@@ -12,15 +12,20 @@ import static org.mockito.Mockito.doAnswer;
 @RunWith(MockitoJUnitRunner.class)
 public class PessoaJuridicaTest {
 
-    @Mock
+    @Spy
     private PessoaJuridica pessoaJuridica;
 
     @Test
     public void testAdicionarCapital() {
+        pessoaJuridica.setCapital(100.0);
+
         doAnswer((Answer) invocation -> {
             Object[] parametros = invocation.getArguments();
             Assert.assertTrue((double) parametros[0] > 10.0);
+            invocation.callRealMethod();
 
+            Assert.assertTrue(pessoaJuridica.getCapital().equals(200.0));
+            
             return null;
         }).when(pessoaJuridica).adicionarCapital(100.0);
 
